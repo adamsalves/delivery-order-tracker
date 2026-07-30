27553595d4777acc8e49467b18cab59dc756cd9c
Adams Alves <alves.adams@gmail.com>
2026-07-30 11:40:16 -0300

    chore: scaffold spring boot api

---
+++ api/src/test/java/dev/adamsalves/ordertracker/OrderTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.adamsalves.ordertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
